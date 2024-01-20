ba9c6ac07f53dfeafdf71cbb4d4b40185d4ab123
Синица Богдан <sinitsa.bogdan.1997@yandex.ru>
2024-01-20 18:04:59 +0300

    add link to GitHub in app.io

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer (props) {
     return (
         <footer className='Footer'>
             <div className="ui-container">
-                <a href="#!" target="_blank">
+                <a href="https://github.com/SinitsaBogdan/app-build-document-to-pdf" target="_blank">
                     Github
                 </a>
             </div>
